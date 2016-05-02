10344d25b7b5292eba8c3539779da8e6112cd811
benjamin dartigues <bdartigues@gmail.com>
2016-05-02 09:42:21 +0200

    remove code from last commit

--- classification/src/new_version/src/set_tulip_params.sh
+++
@@ -1,2 +0,0 @@
-export PYTHONPATH=/mnt/data/opt/tuliprelease/lib/python:$PYTHONPATH
-export LD_LIBRARY_PATH=/mnt/data/opt/tuliprelease/lib/:$LD_LIBRARY_PATH
--- classification/src/new_version/src/tulipcfg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export PATH=/mnt/data/opt/tulipdebug/bin/:$PATH
-export PYTHONPATH=/mnt/data/opt/tulipdebug/lib/python:$PYTHONPATH
-export LD_LIBRARY_PATH=/mnt/data/opt/tulipdebug/lib/:$LD_LIBRARY_PATH
